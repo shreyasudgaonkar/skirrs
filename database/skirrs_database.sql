40b7de0e818610660ffded4d511789a0c63b37a0
shreyasudgaonkar <shreyas.udgaonkar@gmail.com>
2013-09-02 13:11:53 -0700

    Re-ordered the table creation

--- database/skirrs_database.sql
+++ database/skirrs_database.sql
@@ -23,82 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `profile_types`
---
--- Creation: Aug 24, 2013 at 12:07 AM
---
-
-DROP TABLE IF EXISTS `profile_types`;
-CREATE TABLE IF NOT EXISTS `profile_types` (
-  `profile_type_id` int(10) NOT NULL COMMENT 'profile-id',
-  `profile_name` varchar(32) NOT NULL COMMENT 'passenger/driver/dealer, etc.',
-  PRIMARY KEY (`profile_type_id`),
-  UNIQUE KEY `profile_name` (`profile_name`),
-  KEY `profile_type_id` (`profile_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `profile_types`
---
-
-INSERT INTO `profile_types` (`profile_type_id`, `profile_name`) VALUES
-(4, 'dealer'),
-(3, 'driver'),
-(2, 'passenger'),
-(1, 'skirrs_admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rides_offered`
---
--- Creation: Sep 02, 2013 at 06:30 PM
---
-
-DROP TABLE IF EXISTS `rides_offered`;
-CREATE TABLE IF NOT EXISTS `rides_offered` (
-  `ride_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'auto-incremented and primary',
-  `user_id` int(10) NOT NULL COMMENT 'user who posted the ride',
-  `source` varchar(64) NOT NULL COMMENT 'ride from',
-  `destination` varchar(64) NOT NULL COMMENT 'ride to',
-  `departure_date_time` datetime NOT NULL COMMENT 'date and time of departure',
-  `seats_offered` int(10) NOT NULL COMMENT 'no.of available seats',
-  `price` int(10) NOT NULL COMMENT 'price quoted by driver',
-  `pick_up_offered` tinyint(1) NOT NULL COMMENT '0 --> false, 1 --> True',
-  `share_contact_info` tinyint(1) NOT NULL COMMENT '0 --> false, 1 --> True',
-  `ltt` float(10,6) NOT NULL COMMENT 'lattitude',
-  `lng` float(10,6) NOT NULL COMMENT 'longitude',
-  `comments` text NOT NULL COMMENT 'comments about ride/driver',
-  PRIMARY KEY (`ride_id`),
-  KEY `user_id` (`user_id`),
-  KEY `ride_id` (`ride_id`),
-  KEY `ride_id_2` (`ride_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
--- Creation: Aug 23, 2013 at 11:26 PM
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `email_address` varchar(254) NOT NULL,
-  `user_id` int(10) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(26) NOT NULL,
-  `last_name` varchar(26) DEFAULT NULL,
-  `password` varchar(256) NOT NULL,
-  `phone_number` varchar(15) NOT NULL,
-  PRIMARY KEY (`email_address`),
-  UNIQUE KEY `user_id` (`user_id`,`phone_number`),
-  KEY `email_address` (`email_address`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Base user table' AUTO_INCREMENT=46 ;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user_addresses`
 --
 -- Creation: Aug 24, 2013 at 12:57 AM
@@ -114,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `user_addresses` (
   `country` varchar(16) NOT NULL,
   PRIMARY KEY (`user_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -138,8 +62,85 @@ CREATE TABLE IF NOT EXISTS `user_profiles` (
   PRIMARY KEY (`user_id`),
   KEY `user_id` (`user_id`),
   KEY `profile_type_id` (`profile_type_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rides_offered`
+--
+-- Creation: Sep 02, 2013 at 06:30 PM
+--
+
+DROP TABLE IF EXISTS `rides_offered`;
+CREATE TABLE IF NOT EXISTS `rides_offered` (
+  `ride_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'auto-incremented and primary',
+  `user_id` int(10) NOT NULL COMMENT 'user who posted the ride',
+  `source` varchar(128) NOT NULL COMMENT 'ride from',
+  `destination` varchar(128) NOT NULL COMMENT 'ride to',
+  `departure_date_time` datetime NOT NULL COMMENT 'date and time of departure',
+  `seats_offered` int(10) NOT NULL COMMENT 'no.of available seats',
+  `price` int(10) NOT NULL COMMENT 'price quoted by driver',
+  `pick_up_offered` tinyint(1) NOT NULL COMMENT '0 --> false, 1 --> True',
+  `share_contact_info` tinyint(1) NOT NULL COMMENT '0 --> false, 1 --> True',
+  `ltt` float(10,6) NOT NULL COMMENT 'lattitude',
+  `lng` float(10,6) NOT NULL COMMENT 'longitude',
+  `comments` text NOT NULL COMMENT 'comments about ride/driver',
+  PRIMARY KEY (`ride_id`),
+  KEY `user_id` (`user_id`),
+  KEY `ride_id` (`ride_id`),
+  KEY `ride_id_2` (`ride_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Creation: Aug 23, 2013 at 11:26 PM
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `email_address` varchar(254) NOT NULL,
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(26) NOT NULL,
+  `last_name` varchar(26) DEFAULT NULL,
+  `password` varchar(256) NOT NULL,
+  `phone_number` varchar(15) NOT NULL,
+  PRIMARY KEY (`email_address`),
+  UNIQUE KEY `user_id` (`user_id`,`phone_number`),
+  KEY `email_address` (`email_address`)
+) ENGINE=InnoDB COMMENT='Base user table' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+--
+
+-- Table structure for table `profile_types`
+--
+-- Creation: Aug 24, 2013 at 12:07 AM
+--
+
+DROP TABLE IF EXISTS `profile_types`;
+CREATE TABLE IF NOT EXISTS `profile_types` (
+  `profile_type_id` int(10) NOT NULL COMMENT 'profile-id',
+  `profile_name` varchar(32) NOT NULL COMMENT 'passenger/driver/dealer, etc.',
+  PRIMARY KEY (`profile_type_id`),
+  UNIQUE KEY `profile_name` (`profile_name`),
+  KEY `profile_type_id` (`profile_type_id`)
+) ENGINE=InnoDB;
+
+--
+-- Dumping data for table `profile_types`
+--
+
+INSERT INTO `profile_types` (`profile_type_id`, `profile_name`) VALUES
+(4, 'dealer'),
+(3, 'driver'),
+(2, 'passenger'),
+(1, 'skirrs_admin');
+
+-- --------------------------------------------------------
 --
 -- Constraints for dumped tables
 --
